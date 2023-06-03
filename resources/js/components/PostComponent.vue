1ebc2e082d72a88a9687f616633df584b56450f7
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-03 13:06:45 +0300

    Added simple component for a test

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="btn btn-success m-2">
+        Hello World!
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
