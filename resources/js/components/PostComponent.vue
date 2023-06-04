0189ef42cc345e28ee635f602af11798dd70b915
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-04 13:04:34 +0300

    Tested v-for in a component. Used Bootstrap for a table

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,55 +1,58 @@
 <template>
-    <div class="btn btn-success m-2">
-        Post Component
-        <SinglePostComponent></SinglePostComponent>
+    <div class="p-3">
+        <div class="btn btn-success m-2">
+            Post Component
+            <SinglePostComponent></SinglePostComponent>
+        </div>
+        <table class="table table-striped table-dark m-2 w-50">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people" :key="person.id">
+                <th scope="row">{{person.id}}</th>
+                <td>{{person.name}}</td>
+                <td>{{person.age}}</td>
+                <td>{{person.job}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
-    <div class="btn btn-warning m-2">
-        <div>
-            Name: {{name}}
-        </div>
-        <div>
-            Age: {{age}}
-        </div>
-        <div>
-            Is worker: {{isWorker}}
-        </div>
-        <div>
-            Job: {{edJob}}
-        </div>
-    </div>
-    <button class="btn btn-success m-2" @click="sayHello">
-        Hello
-    </button>
-    <button class="btn btn-danger m-2" @click="sayBye">
-        Bye
-    </button>
 </template>
 
 <script>
 import SinglePostComponent from "@/components/SinglePostComponent.vue";
+
 export default {
     name: "PostComponent",
 
     data() {
-      return {
-          name: 'Ed',
-          age: 15,
-          isWorker: false
-      }
-    },
-
-    methods: {
-        sayHello() {
-            console.log('Hello World');
-        },
-        sayBye() {
-            console.log('Goodbye World');
-        }
-    },
-
-    computed: {
-        edJob() {
-            return this.name + ' isn\'t a worker'
+        return {
+            people: [
+                {
+                    id: 1,
+                    name: 'John',
+                    age: 25,
+                    job: 'Developer'
+                },
+                {
+                    id: 2,
+                    name: 'Erick',
+                    age: 30,
+                    job: 'Driver'
+                },
+                {
+                    id: 3,
+                    name: 'Mark',
+                    age: 27,
+                    job: 'Blogger'
+                }
+            ]
         }
     },
 
