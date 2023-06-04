c66adeef62f5fd4f8fdfa5b5b3f9a2b956d1b656
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-04 12:38:50 +0300

    Tested data, methods and computed in a component

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,12 +1,61 @@
 <template>
     <div class="btn btn-success m-2">
-        Hello World!
+        Post Component
+        <SinglePostComponent></SinglePostComponent>
     </div>
+    <div class="btn btn-warning m-2">
+        <div>
+            Name: {{name}}
+        </div>
+        <div>
+            Age: {{age}}
+        </div>
+        <div>
+            Is worker: {{isWorker}}
+        </div>
+        <div>
+            Job: {{edJob}}
+        </div>
+    </div>
+    <button class="btn btn-success m-2" @click="sayHello">
+        Hello
+    </button>
+    <button class="btn btn-danger m-2" @click="sayBye">
+        Bye
+    </button>
 </template>
 
 <script>
+import SinglePostComponent from "@/components/SinglePostComponent.vue";
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+
+    data() {
+      return {
+          name: 'Ed',
+          age: 15,
+          isWorker: false
+      }
+    },
+
+    methods: {
+        sayHello() {
+            console.log('Hello World');
+        },
+        sayBye() {
+            console.log('Goodbye World');
+        }
+    },
+
+    computed: {
+        edJob() {
+            return this.name + ' isn\'t a worker'
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    }
 }
 </script>
 
