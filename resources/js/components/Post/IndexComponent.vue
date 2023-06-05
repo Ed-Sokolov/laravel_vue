4831262313efcc6f865f91cd79b89b672ce5df73
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-05 12:36:28 +0300

    Added IndexComponent for all posts

---
+++ resources/js/components/Post/IndexComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <table class="table table-striped table-dark w-50">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Text</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="post in posts" :key="post.id">
+                <th scope="row">{{ post.id }}</th>
+                <td>{{ post.title }}</td>
+                <td>{{ post.text }}</td>
+                <td>{{ post.created_at }}</td>
+                <td>{{ post.updated_at }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexComponent",
+
+    data() {
+        return {
+            posts: null,
+        }
+    },
+
+    mounted() {
+        this.getPosts()
+    },
+
+    methods: {
+        getPosts() {
+            axios.get('api/posts')
+                .then(response => {
+                    this.posts = response.data;
+                })
+                .catch(error => {
+
+                })
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,66 +2,26 @@
     <div class="p-3">
         <div class="btn btn-success m-2">
             Post Component
-            <SinglePostComponent></SinglePostComponent>
         </div>
         <div class="m-2">
             <CreateComponent></CreateComponent>
         </div>
-        <table class="table table-striped table-dark m-2 w-50">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Text</th>
-                <th scope="col">Created at</th>
-                <th scope="col">Updated at</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="post in posts" :key="post.id">
-                <th scope="row">{{ post.id }}</th>
-                <td>{{ post.title }}</td>
-                <td>{{ post.text }}</td>
-                <td>{{ post.created_at }}</td>
-                <td>{{ post.updated_at }}</td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="m-2">
+            <IndexComponent></IndexComponent>
+        </div>
     </div>
 </template>
 
 <script>
-import SinglePostComponent from "@/components/SinglePostComponent.vue";
 import CreateComponent from "@/components/Post/CreateComponent.vue";
+import IndexComponent from "@/components/Post/IndexComponent.vue";
 
 export default {
     name: "PostComponent",
 
-    data() {
-        return {
-            posts: null,
-        }
-    },
-
-    mounted() {
-        this.getPosts()
-    },
-
-    methods: {
-        getPosts() {
-            axios.get('api/posts')
-                .then(response => {
-                    this.posts = response.data;
-                })
-                .catch(error => {
-
-                })
-        }
-    },
-
     components: {
-        SinglePostComponent,
-        CreateComponent
+        CreateComponent,
+        IndexComponent
     }
 }
 </script>
