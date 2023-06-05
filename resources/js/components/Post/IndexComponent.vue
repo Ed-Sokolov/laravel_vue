c9ad7caf37258c094391c257387ac7c9762a7755
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-05 13:20:01 +0300

    Added a dynamic form for updating a post

--- resources/js/components/Post/IndexComponent.vue
+++ resources/js/components/Post/IndexComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <table class="table table-striped table-dark w-50">
+        <table class="table table-striped table-dark">
             <thead>
             <tr>
                 <th scope="col">#</th>
@@ -8,16 +8,39 @@
                 <th scope="col">Text</th>
                 <th scope="col">Created at</th>
                 <th scope="col">Updated at</th>
+                <th scope="col">Actions</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="post in posts" :key="post.id">
-                <th scope="row">{{ post.id }}</th>
-                <td>{{ post.title }}</td>
-                <td>{{ post.text }}</td>
-                <td>{{ post.created_at }}</td>
-                <td>{{ post.updated_at }}</td>
-            </tr>
+            <template v-for="post in posts" :key="post.id">
+                <tr :class="isEdit(post.id) ? 'd-none' : ''">
+                    <th scope="row">{{ post.id }}</th>
+                    <td>{{ post.title }}</td>
+                    <td>{{ post.text }}</td>
+                    <td>{{ post.created_at }}</td>
+                    <td>{{ post.updated_at }}</td>
+                    <td>
+                        <div class="d-flex flex-column gap-2">
+                            <a href="#" @click.prevent="changeEditPostId(post.id)" class="btn btn-success">Edit</a>
+                        </div>
+                    </td>
+                </tr>
+                <tr :class="isEdit(post.id) ? '' : 'd-none'">
+                    <th scope="row">{{ post.id }}</th>
+                    <td><input type="text" class="form-control" id="title" placeholder="Enter title">
+                    </td>
+                    <td><textarea class="form-control" id="text" rows="3"
+                                  placeholder="Enter text"></textarea></td>
+                    <td>{{ post.created_at }}</td>
+                    <td>{{ post.updated_at }}</td>
+                    <td>
+                        <div class="d-flex flex-column gap-2">
+                            <a href="#" @click.prevent="resetEditPostId" class="btn btn-success">Update</a>
+                            <a href="#" @click.prevent="resetEditPostId" class="btn btn-danger">Cancel</a>
+                        </div>
+                    </td>
+                </tr>
+            </template>
             </tbody>
         </table>
     </div>
@@ -30,6 +53,7 @@ export default {
     data() {
         return {
             posts: null,
+            editPostId: null
         }
     },
 
@@ -46,6 +70,15 @@ export default {
                 .catch(error => {
 
                 })
+        },
+        changeEditPostId(id) {
+            this.editPostId = id;
+        },
+        resetEditPostId() {
+            this.editPostId = null;
+        },
+        isEdit(id) {
+            return this.editPostId === id;
         }
     },
 }
