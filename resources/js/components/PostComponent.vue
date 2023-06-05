db20b35510b951fb105c895b46689edd687f4a65
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-05 11:06:44 +0300

    Added dumpautoload and Post controller. Tested Post/IndexController using axios

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -4,24 +4,7 @@
             Post Component
             <SinglePostComponent></SinglePostComponent>
         </div>
-        <table class="table table-striped table-dark m-2 w-50">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Job</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in peopleAgeLessThan28" :key="person.id">
-                <th scope="row">{{person.id}}</th>
-                <td>{{person.name}}</td>
-                <td>{{person.age}}</td>
-                <td>{{person.job}}</td>
-            </tr>
-            </tbody>
-        </table>
+
     </div>
 </template>
 
@@ -33,48 +16,23 @@ export default {
 
     data() {
         return {
-            people: [
-                {
-                    id: 1,
-                    name: 'John',
-                    age: 25,
-                    job: 'Developer'
-                },
-                {
-                    id: 2,
-                    name: 'Erick',
-                    age: 30,
-                    job: 'Driver'
-                },
-                {
-                    id: 3,
-                    name: 'Mark',
-                    age: 27,
-                    job: 'Blogger'
-                },
-                {
-                    id: 4,
-                    name: 'Olena',
-                    age: 29,
-                    job: 'Teacher'
-                },
-                {
-                    id: 5,
-                    name: 'Ben',
-                    age: 35,
-                    job: 'Actor'
-                }
-            ]
+
+        }
+    },
+
+    mounted() {
+        this.getPosts();
+    },
+
+    methods: {
+        getPosts() {
+            axios.get('/posts').then(response => {
+                console.log(response.data);
+            })
         }
     },
 
     computed: {
-        peopleAgeMoreThan28() {
-            return this.people.filter(person => person.age > 28);
-        },
-        peopleAgeLessThan28() {
-            return this.people.filter(person => person.age < 28);
-        }
     },
 
     components: {
