afaea40ad569aff86c1e5bdb5682f89e70f81e63
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-07 12:41:08 +0300

    Added functions for creating posts and posts list

--- resources/js/components/Post/Create.vue
+++ resources/js/components/Post/Create.vue
@@ -1,12 +1,48 @@
 <template>
+    <div class="mb-3">
+        <a @click.prevent="$router.go(-1)" class="btn btn-outline-primary">Back</a>
+    </div>
     <div>
-        Create
+        <form class="d-flex flex-column gap-3 w-50">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" v-model="title" class="form-control" id="title" placeholder="Enter title">
+            </div>
+            <div class="form-group">
+                <label for="text">Example textarea</label>
+                <textarea v-model="text" class="form-control" id="text" rows="3" placeholder="Enter text"></textarea>
+            </div>
+            <div class="form-group">
+                <button @click.prevent="store" type="submit" class="btn btn-primary">Add</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import router from "@/router.js";
+
 export default {
-    name: "Create"
+    name: "Create",
+
+    data() {
+        return {
+            title: null,
+            text: null
+        }
+    },
+
+    methods: {
+        store() {
+            axios.post('/api/posts', {
+                title: this.title,
+                text: this.text
+            })
+                .then(response => {
+                    router.push({name: 'posts.index'});
+                })
+        }
+    }
 }
 </script>
 
--- resources/js/components/Post/Index.vue
+++ resources/js/components/Post/Index.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
+    <div class="mb-3">
         <RouterLink :to="{ name: 'posts.create' }" class="btn btn-primary">Create post</RouterLink>
     </div>
     <div>
-        Index
+        <table class="table table-striped table-dark">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Text</th>
+                <th scope="col">Category</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="post in posts" :key="post.id">
+                <th scope="row">{{ post.id }}</th>
+                <td>{{ post.title }}</td>
+                <td>{{ post.text }}</td>
+                <td>{{ post.category_id === null ? 'Not has' : post.category_id }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+            posts: null
+        }
+    },
+
+    mounted() {
+        this.index()
+    },
+
+    methods: {
+        index() {
+            axios.get('/api/posts')
+                .then(response => this.posts = response.data)
+        }
+    }
 }
 </script>
 
