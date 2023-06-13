e2defe5a247887cd6cb4d311098b514aaf819ac2
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-13 19:42:06 +0300

    Changed initing authorization in LocalStorage and added a request for refresh token

--- resources/js/config/api/api.js
+++ resources/js/config/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import router from "@/router.js";
 
+const getToken = (name = 'access_token') => localStorage.getItem(name)
+
 const setTokenInConfig = config => {
-    const token = localStorage.getItem('access_token')
+    const token = getToken()
 
     if (token) {
-        config.headers = {
-            'authorization': `Bearer ${token}`
-        }
+        config.headers.authorization = `Bearer ${token}`
     }
 
     return config
@@ -17,18 +17,28 @@ const api = axios.create();
 
 // start request
 
-api.interceptors.request.use(config => {
-    return setTokenInConfig(config)
-}, error => {
+api.interceptors.request.use(config => setTokenInConfig(config), error => {
 })
 
 // end request
 
 // start response
 
-api.interceptors.response.use(config => {
-    return setTokenInConfig(config)
-}, error => {
+api.interceptors.response.use(config => setTokenInConfig(config), error => {
+    if (error.response.data.message === 'Token has expired') {
+        return axios.post('api/auth/refresh', {}, {
+            headers: {
+                'authorization': `Bearer ${getToken()}`
+            }
+        })
+            .then(response => {
+                localStorage.setItem('access_token', response.data.access_token)
+
+                error.config = setTokenInConfig(error.config)
+
+                return api.request(error.config)
+            })
+    }
     if (error.response.status === 401) {
         return router.push({ name: 'auth.login'})
     }
